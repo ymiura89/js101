f28747a9f4c8cb8ae3557e40550e51d79d99e207
Yasu Miura <ymiura89@gmail.com>
2022-12-05 22:13:04 -0500

    Completed pp2

--- lesson_3/practice_problems_2.js
+++ lesson_3/practice_problems_2.js
@@ -44,3 +44,67 @@ let arrayOfNumbers = [1, 2, 3, 4, 5];
 
 arrayOfNumbers.splice(2,1)
 console.log(arrayOfNumbers)
+
+// Problem 6
+//  Suppose we build an array like this:
+
+let flintstones = ["Fred", "Wilma"];
+flintstones.push(["Barney", "Betty"]);
+flintstones.push(["Bambam", "Pebbles"]);
+
+// This code will create a nested array that looks like this:
+
+// ["Fred", "Wilma", ["Barney", "Betty"], ["Bambam", "Pebbles"]];
+
+// Nesting data structures like we do here is commonplace in JavaScript and programming in general. We'll explore this in much greater depth in a future Lesson.
+// 
+// Create a new array that contains all of the above values, but in an un-nested format:
+
+console.log(flintstones.flat());
+
+// Other solutions
+
+let newFlintstones = [];
+flintstones.forEach(element => {
+  newFlintstones = newFlintstones.concat(element);
+});
+
+flintstones = flintstones.reduce((accum, element) => {
+  return accum.concat(element);
+}, []);
+
+flintstones = [].concat(...flintstones);
+
+// Problem 7
+// Create an array from this object that contains only two elements: Barney's name and Barney's number:
+
+flintstones = { Fred: 0, Wilma: 1, Barney: 2, Betty: 3, Bambam: 4, Pebbles: 5 };
+
+let neighbor = Object.entries(flintstones).filter(pair => pair[0] === "Barney").shift();
+
+// Problem 8
+// How would you check whether the objects assigned to variables numbers and table below are arrays?
+
+let numbers1 = [1, 2, 3, 4]; // true
+let table = { field1: 1, field2: 2, field3: 3, field4: 4 }; // false
+
+console.log(Array.isArray(numbers1));
+console.log(Array.isArray(table));
+
+// Problem 9
+// Back in the stone age (before CSS), we used spaces to align things on the screen. If we have a 40-character wide table of Flintstone family members, how can we center the following title above the table with spaces?
+
+let title = "Flintstone Family Members";
+
+let spaceRemaining = Math.floor(40 - title.length);
+
+title.padStart(spaceRemaining + title.length);
+
+// Problem 10
+// Write a one-line expression to count the number of lower-case t characters in each of the following strings:
+
+let statement1 = "The Flintstones Rock!";
+let statement2 = "Easy come, easy go.";
+
+statement1.split('').filter(char => char === 't').length;
+statement2.split('').filter(char => char === 't').length;
